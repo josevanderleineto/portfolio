80a7963aa135fa9e181034af9cf1ad69e87d48f9
Vanderlei Neto <jv.neto@outlook.com.br>
2024-06-30 18:12:22 -0300

    Atualizando footer

--- src/Componets/Footer/Footer.jsx
+++ src/Componets/Footer/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
     return(
         <footer className="Footer">
          <div className="social-icons">
-                <a href="https://www.linkedin.com/in/vanderleineto/"  target="blank"><img src={linkedin} alt="Linkedin" /></a>
+                <a href="https://www.linkedin.com/in/josevanderleineto/"  target="blank"><img src={linkedin} alt="Linkedin" /></a>
                 <a href="https://github.com/josevanderleineto" target="blank"><img src={github} alt="Github" /></a>
                 <a href="https://www.behance.net/josvanderlei1" target="blank"><img src={behance} alt="Bihance" /></a>
-                <a href="https://www.instagram.com/josevanderleineto/" target="blank"><img src={instagram} alt="Instagram" /></a>
+                <a href="https://www.instagram.com/josevanderlei_neto/" target="blank"><img src={instagram} alt="Instagram" /></a>
             </div>
 
         <p >&copy; By Vanderlei Neto</p>
